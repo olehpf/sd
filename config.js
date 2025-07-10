0f8a46c6c1f08b5b445d06c47e576c35cd1f19b3
olehpf <olehpf@gmail.com>
2025-07-10 18:59:51 +0300

    added typography to variables

--- config.js
+++ config.js
@@ -10,6 +10,12 @@ export default {
           format: 'scss/variables',
           filter: (token) => token.filePath.includes('colors.json')
         },
+        {
+          destination: 'typography.scss',
+          format: 'scss/variables',
+          filter: (token) => token.filePath.includes('typography.json')
+        },
+        
         // Add more files here as you create more token files
         // {
         //   destination: 'typography.scss',
@@ -26,6 +32,11 @@ export default {
           destination: 'colors.css',
           format: 'css/variables',
           filter: (token) => token.filePath.includes('colors.json')
+        },
+        {
+          destination: 'typography.css',
+          format: 'css/variables',
+          filter: (token) => token.filePath.includes('typography.json')
         },
         // Add more files here as you create more token files
         // {
